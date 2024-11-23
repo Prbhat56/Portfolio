37ea83f49fc046f7ac355c0d339dd90eb89662be
Prbhat56 <21321@iiitu.ac.in>
2024-11-23 23:13:15 +0530

    completed

---
+++ src/content_option.js
@@ -0,0 +1,153 @@
+import myimage from "../src/assets/images/finalimage.png";
+import img1 from "./assets/images/portfolio.jpg";
+const logotext = "PRABHAT";
+const meta = {
+    title: "Prabhat Kumar",
+    description: "I’m Prabhat Kumar , Full Stack App Developer in Flutter with Firebase and NodeJs",
+};
+
+const introdata = {
+    title: "I’m Prabhat Kumar",
+    animated: {
+        first: "I love Programming",
+        second: "I code in Dart and Java",
+        third: "I develop mobile apps",
+    },
+    description: "my diverse experiences and technical expertise make me a well-rounded and adaptable developer. I am eager to leverage my skills to drive innovation and create impactful solutions in the world of technology",
+    your_img_url: myimage,
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I am a dedicated and passionate full-stack developer with a strong foundation in both front-end and back-end technologies. My experience at Kirabiz Technology allowed me to play a pivotal role in the development of a food delivery app, showcasing my skills in Flutter and Node.js. This internship not only honed my full-stack development abilities but also provided me with the opportunity to create a practical solution for household food delivery businesses",
+};
+const worktimeline = [{
+        jobtitle: "Content Developer",
+        where: "Remote",
+        date: "2022",
+    },
+    {
+        jobtitle: "Full stack app development",
+        where: "Remote",
+        date: "2023",
+    },
+    {
+        jobtitle: "Flutter Developer",
+        where: "Remote ",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Flutter",
+        value: 98,
+    },
+    {
+        name: "Node Js",
+        value: 85,
+    },
+    {
+        name: "C++",
+        value: 80,
+    },
+    {
+        name: "Java",
+        value: 60,
+    },
+    {
+        name: "DBMS",
+        value: 76,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I excel in UI/UX design, creating intuitive and user-friendly interfaces that enhance the overall user experience.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I have a strong background in mobile app development, having successfully completed numerous projects in this field.",
+    },
+    {
+        title: "Backend",
+        description: "I possess the capability to work on backend design, utilizing a range of technologies, including Node.js, MongoDB, Firebase, SQL, and PHP.",
+    },
+    {
+        title: "content developer",
+        description: "I have a knack for content development, creating engaging and informative materials.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://images.unsplash.com/photo-1615828800321-90dcc1a6e775?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZXllJTIwY2FyZXxlbnwwfHwwfHx8MA%3D%3D",
+        description: "I have developed an eyecare app using flutter and machine learning",
+        link: "https://github.com/Prbhat56/Eyecare_app21",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1640340434855-6084b1f4901c?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3RvY2slMjB0cmFkaW5nJTIwYXBwbGljYXRpb258ZW58MHx8MHx8fDA%3D",
+        description: "  I have developed an stock trading using flutter and firebase and machine learning",
+        link: "https://github.com/Prbhat56?tab=repositories",
+    },
+    {
+        img: "https://media.istockphoto.com/id/1358909202/photo/view-of-a-young-female-who-is-shooting-a-red-t-shirt-on-a-table-she-is-into-online-marketing.webp?b=1&s=170667a&w=0&k=20&c=B2U5XRgy_EwnOXc88aZ9lCd68bCWmUZ_AIpwl2pgBIw=",
+        description: "This was my first project in flutter it deals with old cloth here person can sell old clothes online and buy both",
+        link: "https://github.com/Prbhat56/cloth_selling_app1",
+    },
+    {
+        img: "https://media.istockphoto.com/id/1454104829/photo/love-is-a-game-of-tic-tac-toe-constantly-waiting-for-the-next-x-or-o.webp?b=1&s=170667a&w=0&k=20&c=zpTziugB94lcG7o7X4utIXy3eoYi8Vvsr3MBrCE-TJw=",
+        description: "i have developed a tic tac toe game app using socket flutter, node js a backend",
+        link: "https://github.com/Prbhat56/Tic_Tac_Toe",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "I have developed an food delivery app using node js and flutter as frontend framework",
+        link: "https://github.com/SumitSatre/Food-Delivery-Flutter-App",
+    },
+    {
+        img: "https://media.istockphoto.com/id/1201563119/photo/food-delivery-man-getting-ready-for-work.webp?b=1&s=170667a&w=0&k=20&c=Hp64FuGkgl-LesSN_bq_YM2DyAvT7mIZMZ29ATvv-Q0=",
+        description: "It is an project that was developed using flutter and it was assigned during my intern , it used for ordering online food through household",
+        link: "https://github.com/SvapakIndia/Food-Delivery-App",
+    },
+    {
+        img: "https://media.istockphoto.com/id/905234844/photo/listening-music-smart-phone-connected-to-car-audio-system.webp?b=1&s=170667a&w=0&k=20&c=F2u9uO3VPRB1GhEWDo3J64hay4tBQ-hVmbDtsJReKeQ=",
+        description: "It is an project which used to extract all mp3 files from an phone and put at one palce and person can listen all of it dveloped using getx and flutter framework ",
+        link: "https://github.com/Prbhat56/Music_App",
+    },
+    {
+        img: "https://media.istockphoto.com/id/1142218956/photo/medical-technology-and-doctor-staff-service-concept.webp?b=1&s=170667a&w=0&k=20&c=9rySKA3DYMMhmqmKl5ySlZvZnu1ExdiHZp3jAm_u58Y=",
+        description: "Currently I am working on this project as an flutter deevloper in the intern and we are building a website where a village people can acess medical facilities online ",
+        link: "https://github.com/Prbhat56/Doctor_Booking_App",
+    },
+
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hendramyuv@gmail.com",
+    YOUR_FONE: "+919693525118",
+    description: "I am Prabhat Kumar and if you have any query related to development and data structure feel free contact me  ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Prbhat56",
+   
+    linkedin: "https://www.linkedin.com/in/prabhat-kumar-iiitu-64a466229/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
